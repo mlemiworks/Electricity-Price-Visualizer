3321c0022190671219d53f3327c22cf9c24f953a
Markus Lemiläinen <markus.lemilainen@edu.karelia.fi>
2024-10-06 14:57:23 +0300

    ditched the weather widget for now. separate button for switching between today and tomorrow, few ui tweaks

--- powerBackEnd/index.js
+++ powerBackEnd/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const fetch = require("node-fetch");
 const cron = require("node-cron");
+const moment = require("moment-timezone");
 const NodeCache = require("node-cache");
 const { formatDatesForApi } = require("./utils/dateFormatter");
 const { parseXMLtoObject } = require("./utils/dataParser");
@@ -12,15 +13,16 @@ const app = express();
 app.use(cors());
 app.use(express.static("dist"));
 
-// Cache for the data, 60 minutes, scheduled to fetch new data every hour
-const dataCache = new NodeCache({ stdTTL: 3600 }); // 60 minutes cache
+// Cache for the data
+const dataCache = new NodeCache({ stdTTL: 0 }); // 0 means infinite time to live
 
 const fetchData = async () => {
+  // empty cache, otherwise yesterday's data will be shown
+  // as tomorrows data when day changes, and new data is not available yet
+  dataCache.del("priceData");
+
   const [periodStart, periodEnd] = await formatDatesForApi();
   const apiKey = process.env.VITE_API_KEY;
-
-  console.log("periodStart", periodStart);
-  console.log("periodEnd", periodEnd);
 
   const url = `https://web-api.tp.entsoe.eu/api?documentType=A44&out_Domain=10YFI-1--------U&in_Domain=10YFI-1--------U&periodStart=${periodStart}&periodEnd=${periodEnd}&securityToken=${apiKey}`;
 
@@ -29,14 +31,21 @@ const fetchData = async () => {
   const parsedData = await parseXMLtoObject(xmlData); // Parse XML to JS object
 
   dataCache.set("priceData", parsedData);
-  console.log(parsedData)
 };
 
 // Initial fetch
 fetchData();
 
-// Scheduler to fetch data every hour
-cron.schedule("0 * * * *", fetchData);
+cron.schedule(
+  "0 14 * * *",
+  () => {
+    fetchData();
+  },
+  {
+    scheduled: true,
+    timezone: "Europe/Helsinki", // Set the timezone for the schedule
+  }
+);
 
 // API endpoint, if data is not in cache, fetch it
 app.get("/api", async (req, res) => {
